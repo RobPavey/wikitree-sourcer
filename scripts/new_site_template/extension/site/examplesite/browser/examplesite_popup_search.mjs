c55297b4b77852cc3c5189a4996e5c6773b980e6
Rob Pavey <robpavey@gmail.com>
2022-10-04 13:55:05 -0700

    Add script to create new site

---
+++ scripts/new_site_template/extension/site/examplesite/browser/examplesite_popup_search.mjs
@@ -0,0 +1,158 @@
+/*
+MIT License
+
+Copyright (c) 2020 Robert M Pavey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+import {
+  addSameRecordMenuItem,
+  addBackMenuItem,
+  addMenuItem,
+  beginMainMenu,
+  endMainMenu,
+  doAsyncActionWithCatch
+} from "/base/browser/popup/popup_menu_building.mjs";
+
+import { doSearch, registerSearchMenuItemFunction, testFilterForDatesAndCountries } from "/base/browser/popup/popup_search.mjs";
+
+import { options } from "/base/browser/options/options_loader.mjs";
+
+const examplesiteStartYear = 1837;
+const examplesiteEndYear = 1992;
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Menu actions
+//////////////////////////////////////////////////////////////////////////////////////////
+
+async function examplesiteSearch(generalizedData, typeOfSearch) {
+  const input = { typeOfSearch: typeOfSearch, generalizedData: generalizedData, options: options }
+  doAsyncActionWithCatch("ExampleSite Search", input, async function() {
+    let loadedModule = await import(`../core/examplesite_build_search_url.mjs`);
+    doSearch(loadedModule, input);
+  });
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Menu items
+//////////////////////////////////////////////////////////////////////////////////////////
+
+function addExamplesiteDefaultSearchMenuItem(menu, data, backFunction, filter) {
+  //console.log("addExamplesiteDefaultSearchMenuItem, data is:");
+  //console.log(data);
+
+  const stdCountryName = "England and Wales";
+  
+  if (filter) {
+    if (!testFilterForDatesAndCountries(filter, examplesiteStartYear, examplesiteEndYear, [stdCountryName])) {
+      return;
+    }
+  }
+  else {
+    let maxLifespan = Number(options.search_general_maxLifespan);
+    let birthPossibleInRange = data.generalizedData.couldPersonHaveBeenBornInDateRange(examplesiteStartYear, examplesiteEndYear, maxLifespan);
+    let deathPossibleInRange = data.generalizedData.couldPersonHaveDiedInDateRange(examplesiteStartYear, examplesiteEndYear, maxLifespan);
+    let marriagePossibleInRange = data.generalizedData.couldPersonHaveMarriedInDateRange(examplesiteStartYear, examplesiteEndYear, maxLifespan);
+
+    if (!(birthPossibleInRange || deathPossibleInRange || marriagePossibleInRange)) {
+      //console.log("addExamplesiteDefaultSearchMenuItem: dates not in range");
+      return;
+    }
+
+    if (!data.generalizedData.didPersonLiveInCountryList([stdCountryName])) {
+      //console.log("addExamplesiteDefaultSearchMenuItem: didPersonLiveInCountryList returned false");
+      return;
+    }
+  }
+  
+  addMenuItem(menu, "Search ExampleSite...", function(element) {
+    setupExamplesiteSearchSubMenu(data, backFunction, filter);
+  });
+
+  return true;
+}
+
+async function addExamplesiteSameRecordMenuItem(menu, data) {
+  await addSameRecordMenuItem(menu, data, "examplesite", function(element) {
+    examplesiteSearch(data.generalizedData, "SameCollection");
+  });
+}
+
+function addExamplesiteSearchBirthsMenuItem(menu, data, filter) {
+  if (!filter) {
+    let maxLifespan = Number(options.search_general_maxLifespan);
+    let birthPossibleInRange = data.generalizedData.couldPersonHaveBeenBornInDateRange(examplesiteStartYear, examplesiteEndYear, maxLifespan);
+    if (!birthPossibleInRange) {
+      return;
+    }
+  }
+  addMenuItem(menu, "Search ExampleSite Births", function(element) {
+    examplesiteSearch(data.generalizedData, "Births");
+  });
+}
+
+function addExamplesiteSearchMarriagesMenuItem(menu, data, filter) {
+  if (!filter) {
+    let maxLifespan = Number(options.search_general_maxLifespan);
+    let marriagePossibleInRange = data.generalizedData.couldPersonHaveMarriedInDateRange(examplesiteStartYear, examplesiteEndYear, maxLifespan);
+    if (!marriagePossibleInRange) {
+      return;
+    }
+  }
+  addMenuItem(menu, "Search ExampleSite Marriages", function(element) {
+    examplesiteSearch(data.generalizedData, "Marriages");
+  });
+}
+
+function addExamplesiteSearchDeathsMenuItem(menu, data, filter) {
+  if (!filter) {
+    let maxLifespan = Number(options.search_general_maxLifespan);
+    let deathPossibleInRange = data.generalizedData.couldPersonHaveDiedInDateRange(examplesiteStartYear, examplesiteEndYear, maxLifespan);
+    if (!deathPossibleInRange) {
+      return;
+    }
+  }
+  addMenuItem(menu, "Search ExampleSite Deaths", function(element) {
+    examplesiteSearch(data.generalizedData, "Deaths");
+  });
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Submenus
+//////////////////////////////////////////////////////////////////////////////////////////
+
+async function setupExamplesiteSearchSubMenu(data, backFunction, filter) {
+  let menu = beginMainMenu();
+
+  addBackMenuItem(menu, backFunction);
+
+  await addExamplesiteSameRecordMenuItem(menu, data, filter);
+  addExamplesiteSearchBirthsMenuItem(menu, data, filter);
+  addExamplesiteSearchMarriagesMenuItem(menu, data, filter);
+  addExamplesiteSearchDeathsMenuItem(menu, data, filter);
+
+  endMainMenu(menu);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Register the search menu - it can be used on the popup for lots of sites
+//////////////////////////////////////////////////////////////////////////////////////////
+
+registerSearchMenuItemFunction("examplesite", "ExampleSite", addExamplesiteDefaultSearchMenuItem);
