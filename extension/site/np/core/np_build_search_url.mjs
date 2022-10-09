ae77fed5e11e2739bf9a9d09dbbd7e05e3b4bac0
Savanna King <savanna18king@gmail.com>
2022-10-09 17:33:14 -0500

    add tests

---
+++ extension/site/np/core/np_build_search_url.mjs
@@ -0,0 +1,57 @@
+/*
+MIT License
+
+Copyright (c) 2020 Robert M Pavey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+import { NpUriBuilder } from "./np_uri_builder.mjs";
+
+function buildSearchUrl(buildUrlInput) {
+
+  const data = buildUrlInput.generalizedData;
+  const options = buildUrlInput.options;
+
+  var builder = new NpUriBuilder;
+
+  builder.addSearchParameter("query", data.inferFirstName() ?? "" + " " + data.inferMiddleName() ?? "" + " " + data.inferLastName() ?? "")
+
+  let birthYear = data.inferBirthYear()
+  let deathYear = data.inferDeathYear()
+  if (birthYear && deathYear) {
+    builder.addSearchParameter("dr_year", birthYear + "-" + deathYear)
+  } else if (birthYear) {
+    builder.addSearchParameter("dr_year", birthYear + "-" + (birthYear + 80))
+  } else if (deathYear) {
+    builder.addSearchParameter("dr_year", (deathYear - 80) + "-" + deathYear)
+  }
+
+  const url = builder.getUri();
+
+  //console.log("URL is " + url);
+
+  var result = {
+    'url': url,
+  }
+
+  return result;
+}
+
+export { buildSearchUrl };
---
+++ extension/site/np/core/np_options.mjs
@@ -0,0 +1,56 @@
+/*
+MIT License
+
+Copyright (c) 2020 Robert M Pavey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+import {
+  registerSubsectionForOptions,
+  registerSubheadingForOptions,
+  registerOptionsGroup,
+  registerSiteSearchPopupOptionsGroup,
+} from "../../../base/core/options/options_registry.mjs";
+
+const searchOptionsGroup = {
+  category: "search",
+  subcategory: "np",
+  tab: "search",
+  subsection: "np",
+  subheading: "parameters",
+  options: [
+  ],
+}
+
+const citationOptionsGroup = {
+  category: "citation",
+  subcategory: "np",
+  tab: "citation",
+  subsection: "np",
+  options: [
+  ],
+};
+
+// registerSubsectionForOptions("search", "np", "Newspapers.com");
+// registerSiteSearchPopupOptionsGroup("np", 4, 4);
+// registerSubheadingForOptions("search", "np", "parameters", "Search Parameters");
+// registerOptionsGroup(searchOptionsGroup);
+// registerSubsectionForOptions("citation", "np", "Newspapers.com");
+// registerOptionsGroup(citationOptionsGroup);
