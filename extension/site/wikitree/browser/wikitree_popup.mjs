599ef8ec2c2e1b161408cd1d19ca0909ee7ca9d0
Rob Pavey <robpavey@gmail.com>
2023-10-06 13:28:30 -0700

    wikitree: remove API use except for add person

--- extension/site/wikitree/browser/wikitree_popup.mjs
+++ extension/site/wikitree/browser/wikitree_popup.mjs
@@ -86,6 +86,10 @@ async function makeApiRequests(extractedData) {
       );
     }
   } else {
+    /*
+      Currently we only use waitForAPIResponse in the EditFamily case
+      so no need to request API data for any other case until we have a use for it.
+
     const fields = "Id,Gender,Name,FirstName,LastNameAtBirth";
     wtApiGetPerson(extractedData.wikiId, fields).then(
       function handleResolve(jsonData) {
@@ -99,6 +103,7 @@ async function makeApiRequests(extractedData) {
         // nothing to do here
       }
     );
+    */
   }
 }
 
