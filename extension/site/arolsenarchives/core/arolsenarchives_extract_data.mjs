677e61a462c9e20cc820d3ac256e8cc81a745476
Lukas <baulukas1301@googlemail.com>
2025-10-25 20:33:34 +0200

    start adding tests for arolsen archives

--- extension/site/arolsenarchives/core/arolsenarchives_extract_data.mjs
+++ extension/site/arolsenarchives/core/arolsenarchives_extract_data.mjs
@@ -63,10 +63,10 @@ function extractData(document, url) {
   const breadcrum_item = document.querySelector('div[class="fd-tree-path"]');
   let breadcrum_string = "";
   for (let breadcrum of breadcrum_item.querySelectorAll('a[class="ng-star-inserted"]')) {
-    breadcrum_string += ", " + breadcrum.text;
+    breadcrum_string += ", " + breadcrum.text.trim();
   }
-  const title = document.querySelector('h1[class="title ng-tns-c171-3"');
-  breadcrum_string += ", " + title.text;
+  const title = document.querySelector('h1[class="title ng-tns-c171-3"]');
+  breadcrum_string += ", " + title.textContent.trim();
   result.breadcrum = breadcrum_string.substring(2);
 
   const selected_index = getSelectedRow(document);
--- unit_tests/arolsenarchives/arolsenarchives_test_content_and_citation.mjs
+++ unit_tests/arolsenarchives/arolsenarchives_test_content_and_citation.mjs
@@ -31,12 +31,10 @@ import { runGeneralizeDataTests } from "../test_utils/test_generalize_data_utils
 import { runBuildCitationTests } from "../test_utils/test_build_citation_utils.mjs";
 
 const regressionData = [
-  /*
   {
-    caseName: "b_1902_calvert_florence",
-    url: "https://www.arolsenarchives.org.uk/cgi/information.pl?r=107280059:7282&d=bmd_1649064119",
-  },
-  */
+    caseName: "71022005",
+    url: "https://collections.arolsen-archives.org/de/document/71022005",
+  }
 ];
 
 async function runTests(testManager) {
