45fb8bfa56ead928a1ba0256d3419c5e31778330
rickpswbellnet <rpetersongen@gmail.com>
2026-01-20 19:56:22 -0600

    additional morbihan site unit test cases

--- unit_tests/morbihan/morbihan_test_content_and_citation.mjs
+++ unit_tests/morbihan/morbihan_test_content_and_citation.mjs
@@ -44,8 +44,32 @@ const regressionData = [
     url: "https://rechercher.patrimoines-archives.morbihan.fr/ark:/15049/3541873/daogrp/0/37",
   },
   {
+    caseName: "guiscriff_081_1miec081_r13-0004_-_naissance_mariage_décès_-_1868-1873_img_152",
+    url: "https://rechercher.patrimoines-archives.morbihan.fr/ark:/15049/vta54487e7f49ee3/daogrp/0/152",
+  },
+  {
     caseName: "langonnet_-_recensement_1901_3_es_100_img_51",
     url: "https://rechercher.patrimoines-archives.morbihan.fr/ark:/15049/1542989/daogrp/0/51",
+  },
+  {
+    caseName: "langonnet_100_1miec100_r7_01-0001_-_baptême_mariage_sépulture_-_1700-1762_img_656",
+    url: "https://rechercher.patrimoines-archives.morbihan.fr/ark:/15049/vta25b38c313cf21afa/daogrp/0/656",
+  },
+  {
+    caseName: "lanouée_102_1miec102_r6_01-0001_-_baptême_mariage_-_1747-1769_img_175",
+    url: "https://rechercher.patrimoines-archives.morbihan.fr/ark:/15049/vta88cd47927f0ddf71/daogrp/0/175",
+  },
+  {
+    caseName: "lorient_121_1miec121_r07-0096_-_baptême_mariage_sépulture_-_1732-1732_img_1",
+    url: "https://rechercher.patrimoines-archives.morbihan.fr/ark:/15049/vta7511557eb3a4b76b/daogrp/0/1",
+  },
+  {
+    caseName: "port-louis_ec_181-0098_-_décès_-_1803-1804_img_1",
+    url: "https://rechercher.patrimoines-archives.morbihan.fr/ark:/15049/vta5448861531bdd/daogrp/0/1",
+  },
+  {
+    caseName: "roudouallec_4e_199_009-0253_-_naissance_mariage_décès_-_1872-1872_img_6",
+    url: "https://rechercher.patrimoines-archives.morbihan.fr/ark:/15049/vta54488725a1b86/daogrp/0/6",
   },
   /*
   {
