1c98cf72c1617981806e1a1c938ce33c66641370
Rob Pavey <robpavey@gmail.com>
2026-03-10 13:58:15 -0700

    options: switch to a new compressed format because I hit the quota for item size

--- extension/base/browser/options/options_storage.mjs
+++ extension/base/browser/options/options_storage.mjs
@@ -42,6 +42,14 @@ SOFTWARE.
 //
 // The options version was bumped to 5 as part of this change.
 
+async function displayStatusMessage(message, ms) {
+  var status = document.getElementById("status");
+  status.textContent = message;
+  setTimeout(function () {
+    status.textContent = "";
+  }, ms);
+}
+
 function putNewLoadedOptionsSetInOptions(options, optionsSet, prefix) {
   if (!optionsSet || !options || !prefix) {
     return;
@@ -54,11 +62,60 @@ function putNewLoadedOptionsSetInOptions(options, optionsSet, prefix) {
   }
 }
 
+function loadCompressedOptions(items) {
+  let loadedOptions = {};
+
+  let keyLookupTable = items.options_keyLUT;
+  let valueLookupTable = items.options_valueLUT;
+  if (!keyLookupTable || !valueLookupTable) {
+    console.log("ERROR: loadCompressedOptions, lookup table missing");
+    return;
+  }
+
+  const prefixes = ["options", "search", "citation", "narrative", "table", "buildAll", "addMerge", "context"];
+
+  for (let prefix of prefixes) {
+    let itemKey = "options_" + prefix;
+    let item = items[itemKey];
+    if (!item) {
+      continue;
+    }
+
+    for (let compressedKey of Object.keys(item)) {
+      let compressedValue = item[compressedKey];
+      let compressedKeyParts = compressedKey.split("_");
+      let key = prefix;
+      for (let compressedPart of compressedKeyParts) {
+        let keyLookupIndex = parseInt(compressedPart, 16);
+        let part = keyLookupTable[keyLookupIndex];
+        if (part === undefined) {
+          console.log("ERROR: key part not found in lookup table: " + compressedValue);
+        } else {
+          key += "_" + part;
+        }
+      }
+      let valueLookupIndex = parseInt(compressedValue, 16);
+      let jsonValue = valueLookupTable[valueLookupIndex];
+      if (jsonValue === undefined) {
+        console.log("ERROR: value not found in lookup table: " + compressedValue);
+      } else {
+        let value = JSON.parse(jsonValue);
+        loadedOptions[key] = value;
+      }
+    }
+  }
+
+  //console.log("decompressed options are:");
+  //console.log(loadedOptions);
+
+  return loadedOptions;
+}
+
 async function loadOptions() {
   let loadedOptions = undefined;
 
   chrome.storage.sync.getBytesInUse(null, function (bytesInUse) {
-    //console.log("loadOptions: total sync storage in use is : " + bytesInUse);
+    console.log("loadOptions: total sync storage in use is : " + bytesInUse);
   });
 
   let itemsNew = await chrome.storage.sync.get([
@@ -71,12 +128,19 @@ async function loadOptions() {
     "options_addMerge", // replaced addPerson in options version 6
     "options_context",
     "options_version",
+    // new in compressed form
+    "options_options",
+    "options_keyLUT",
+    "options_valueLUT",
   ]);
 
   //console.log("loadOptions: itemsNew is");
   //console.log(itemsNew);
 
-  if (itemsNew && itemsNew.options_search) {
+  if (itemsNew && itemsNew.options_keyLUT) {
+    // options are compressed
+    loadedOptions = loadCompressedOptions(itemsNew);
+  } else if (itemsNew && itemsNew.options_search) {
     loadedOptions = {};
     putNewLoadedOptionsSetInOptions(loadedOptions, itemsNew.options_search, "search");
     putNewLoadedOptionsSetInOptions(loadedOptions, itemsNew.options_citation, "citation");
@@ -107,8 +171,11 @@ async function loadOptions() {
   return loadedOptions;
 }
 
-function convertOptionsObjectToSaveFormat(options) {
-  const prefixes = ["search", "citation", "narrative", "table", "buildAll", "addMerge", "context", "options_version"];
+function convertOptionsObjectToSaveFormatCompressed(options) {
+  //console.log("convertOptionsObjectToSaveFormat, options is:");
+  //console.log(options);
+
+  const prefixes = ["options", "search", "citation", "narrative", "table", "buildAll", "addMerge", "context"];
 
   let newOptions = {};
 
@@ -116,21 +183,72 @@ function convertOptionsObjectToSaveFormat(options) {
     newOptions[prefix] = {};
   }
 
+  let keyLookupTable = [];
+  let valueLookupTable = [];
+  valueLookupTable.push("false");
+  valueLookupTable.push("true");
+  valueLookupTable.push('"none"');
+  valueLookupTable.push('"auto"');
+
+  function compressKey(value) {
+    let parts = value.split("_");
+    let newKey = "";
+    for (let part of parts) {
+      let partNum = 0;
+      let index = keyLookupTable.indexOf(part);
+      if (index != -1) {
+        partNum = index;
+      } else {
+        partNum = keyLookupTable.length;
+        keyLookupTable.push(part);
+      }
+      if (newKey) {
+        newKey += "_";
+      }
+      newKey += partNum.toString(16);
+    }
+    return newKey;
+  }
+
+  function compressValue(value) {
+    let lookupIndex = 0;
+    let index = valueLookupTable.indexOf(value);
+    if (index != -1) {
+      lookupIndex = index;
+    } else {
+      lookupIndex = valueLookupTable.length;
+      valueLookupTable.push(value);
+    }
+    let newValue = lookupIndex.toString(16);
+    return newValue;
+  }
+
   const keys = Object.keys(options);
   for (let key of keys) {
+    let value = options[key];
+    let stringValue = JSON.stringify(value);
+    let compressedValue = compressValue(stringValue);
     let underscoreIndex = key.indexOf("_");
     if (underscoreIndex != -1) {
       let prefix = key.substring(0, underscoreIndex);
       if (prefixes.includes(prefix)) {
         let remainder = key.substring(underscoreIndex + 1);
-        newOptions[prefix][remainder] = options[key];
+        let newKey = compressKey(remainder);
+        newOptions[prefix][newKey] = compressedValue;
       } else {
-        newOptions[key] = options[key];
+        let newKey = compressKey(key);
+        newOptions[newKey] = compressedValue;
       }
     } else {
-      newOptions[key] = options[key];
+      newOptions[key] = compressedValue;
     }
   }
+
+  newOptions.keyLUT = keyLookupTable;
+  newOptions.valueLUT = valueLookupTable;
+
+  //console.log("convertOptionsObjectToSaveFormat, newOptions is:");
+  //console.log(newOptions);
 
   return newOptions;
 }
@@ -140,7 +258,29 @@ async function saveOptions(options) {
   //console.log("saveOptions: options is:");
   //console.log(options);
 
-  let saveFormatOptions = convertOptionsObjectToSaveFormat(options);
+  //let saveFormatOptions = convertOptionsObjectToSaveFormat(options);
+  let saveFormatOptions = convertOptionsObjectToSaveFormatCompressed(options);
+
+  /*
+  let textEncoder = new TextEncoder();
+  let totalSize = 0;
+  let totalBytes = 0;
+  for (let key of Object.keys(saveFormatOptions)) {
+    let value = saveFormatOptions[key];
+    let string = JSON.stringify(value);
+    console.log("saveFormatOptions." + key + " length is: " + string.length);
+    totalSize += string.length;
+    const estimatedBytes = key.length + textEncoder.encode(string).length;
+    console.log("saveFormatOptions." + key + " estimatedBytes is: " + estimatedBytes);
+    totalBytes += estimatedBytes;
+  }
+  console.log("total length is: " + totalSize + ", max total is: " + chrome.storage.sync.QUOTA_BYTES);
+  console.log("total estimatedBytes is: " + totalBytes + ", max total is: " + chrome.storage.sync.QUOTA_BYTES);
+
+  chrome.storage.sync.getBytesInUse(null, function (bytesInUse) {
+    console.log("saveOptions: total sync storage in use is : " + bytesInUse);
+  });
+  */
 
   //console.log("saveOptions: saveFormatOptions is:");
   //console.log(saveFormatOptions);
@@ -156,19 +296,25 @@ async function saveOptions(options) {
         options_buildAll: saveFormatOptions.buildAll,
         options_addMerge: saveFormatOptions.addMerge,
         options_context: saveFormatOptions.context,
-        options_version: saveFormatOptions.options_version,
+        options_options: saveFormatOptions.options,
+        options_keyLUT: saveFormatOptions.keyLUT,
+        options_valueLUT: saveFormatOptions.valueLUT,
       },
       function () {
         if (!chrome.runtime.lastError) {
           // Update status to let user know options were saved.
-          var status = document.getElementById("status");
-          status.textContent = "Options saved.";
-          setTimeout(function () {
-            status.textContent = "";
-          }, 750);
+          displayStatusMessage("Options saved.", 750);
         } else {
           console.log("saveOptions: Runtime error is:");
           console.log(chrome.runtime.lastError);
+
+          if (chrome.runtime.lastError.message == "Resource::kQuotaBytesPerItem quota exceeded") {
+            for (let key of Object.keys(saveFormatOptions)) {
+              let value = saveFormatOptions[key];
+              let string = JSON.stringify(value);
+              console.log("saveFormatOptions." + key + " length is: " + string.length);
+            }
+          }
 
           chrome.storage.sync.getBytesInUse(null, function (bytesInUse) {
             console.log("saveOptions: total sync storage in use is : " + bytesInUse);
@@ -178,43 +324,43 @@ async function saveOptions(options) {
           // The saved failed, clear all sync storage and try again.
           // Since this extension ONLY uses the sync storage area for user options it should not lose
           // data unless the second save fails also.
-          chrome.storage.sync.clear(function () {
-            chrome.storage.sync.set(
-              {
-                options_search: saveFormatOptions.search,
-                options_citation: saveFormatOptions.citation,
-                options_narrative: saveFormatOptions.narrative,
-                options_table: saveFormatOptions.table,
-                options_buildAll: saveFormatOptions.buildAll,
-                options_addMerge: saveFormatOptions.addMerge,
-                options_context: saveFormatOptions.context,
-                options: saveFormatOptions.options,
-              },
-              function () {
-                if (!chrome.runtime.lastError) {
-                  // Update status to let user know options were saved.
-                  var status = document.getElementById("status");
-                  status.textContent = "Options saved.";
-                  setTimeout(function () {
-                    status.textContent = "";
-                  }, 750);
-                } else {
-                  console.log("saveOptions, after clear: Runtime error is:");
-                  console.log(chrome.runtime.lastError);
+          // NOTE: this caused an issue when I exceeded the quota for one item. The required fix was to
+          // change the code but this bit of code deleted all my options!
+          if (chrome.runtime.lastError.message == "Resource::kQuotaBytes quota exceeded") {
+            chrome.storage.sync.clear(function () {
+              chrome.storage.sync.set(
+                {
+                  options_search: saveFormatOptions.search,
+                  options_citation: saveFormatOptions.citation,
+                  options_narrative: saveFormatOptions.narrative,
+                  options_table: saveFormatOptions.table,
+                  options_buildAll: saveFormatOptions.buildAll,
+                  options_addMerge: saveFormatOptions.addMerge,
+                  options_context: saveFormatOptions.context,
+                  options_options: saveFormatOptions.options,
+                  options_keyLUT: saveFormatOptions.keyLUT,
+                  options_valueLUT: saveFormatOptions.valueLUT,
+                },
+                function () {
+                  if (!chrome.runtime.lastError) {
+                    // Update status to let user know options were saved.
+                    var status = document.getElementById("status", 750);
+                  } else {
+                    console.log("saveOptions, after clear: Runtime error is:");
+                    console.log(chrome.runtime.lastError);
 
-                  chrome.storage.sync.getBytesInUse(null, function (bytesInUse) {
-                    console.log("saveOptions, after clear: total sync storage in use is : " + bytesInUse);
-                  });
+                    chrome.storage.sync.getBytesInUse(null, function (bytesInUse) {
+                      console.log("saveOptions, after clear: total sync storage in use is : " + bytesInUse);
+                    });
 
-                  var status = document.getElementById("status");
-                  status.textContent = "Options could not be saved.";
-                  setTimeout(function () {
-                    status.textContent = "";
-                  }, 5000);
+                    displayStatusMessage("Options could not be saved.", 5000);
+                  }
                 }
-              }
-            );
-          });
+              );
+            });
+          } else {
+            displayStatusMessage("Options could not be saved.", 5000);
+          }
         }
       }
     );
@@ -226,11 +372,7 @@ async function saveOptions(options) {
       console.log("saveOptions: total sync storage in use is : " + bytesInUse);
     });
 
-    var status = document.getElementById("status");
-    status.textContent = "Options could not be saved.";
-    setTimeout(function () {
-      status.textContent = "";
-    }, 5000);
+    displayStatusMessage("Options could not be saved.", 5000);
   }
 }
 
