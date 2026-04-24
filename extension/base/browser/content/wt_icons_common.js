0d863f8f3a32bc1244dd45731b41a9f37f815d14
Rob Pavey <robpavey@gmail.com>
2026-04-24 10:33:35 -0700

    wt icons: tooltip styling

--- extension/base/browser/content/wt_icons_common.js
+++ extension/base/browser/content/wt_icons_common.js
@@ -1129,16 +1129,39 @@ class WikiTreeSourcerPageModsHelper {
           margin: 0 0 4px 0;
         } 
         .wt-sourcer-custom-tooltip ul {
-          margin: 5px 0 0 1.2em; /* Use em for bullet spacing */
+          margin: 0;
           padding: 0;
           list-style-position: outside;
+          padding-inline-start: 0;
+          display: flex;
+          flex-direction: column;
+          gap: 4px;
         }
         .wt-sourcer-custom-tooltip li {
           /* 3. Force the LI to start at the edge of the UL's padding */
           margin: 0 0 2px 0;
-          padding-left: 4px;
-          text-indent: -8px;
+          padding-left: 0px;
+          /*text-indent: -8px;*/
+
+          display: grid;
+          /* Grid column 1 is the bullet, column 2 is the text */
+          grid-template-columns: 1em auto; 
+          align-items: start;
+          width: fit-content;
+          line-height: 1.4;
         }
+
+        /* Re-insert a custom bullet as a pseudo-element */
+        .wt-sourcer-custom-tooltip li::before {
+            margin: 0;
+            content: "•";
+            grid-column: 1;
+            display: inline-block;
+            /* This ensures the bullet is vertically centered with the first line */
+            text-align: left; 
+            /*color: #bbb; /* Subtle bullet color */
+        }
+
         .wt-sourcer-tooltip-error {
           color: red;
         }
