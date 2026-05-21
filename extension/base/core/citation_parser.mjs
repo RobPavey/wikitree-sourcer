1603f0b3078ad5c5b0f6479097042ee8883d56a2
Rob Pavey <robpavey@gmail.com>
2026-05-21 10:31:33 -0700

    context menu: refactor to more site specific details out of base background code

---
+++ extension/base/core/citation_parser.mjs
@@ -0,0 +1,248 @@
+/*
+MIT License
+
+Copyright (c) 2020 Robert M Pavey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+class CitationParser {
+  constructor(text) {
+    this.text = text;
+  }
+
+  extractYearAndRegistrationNumberFromText(extractInput) {
+    if (!this.text) {
+      return undefined;
+    }
+
+    let lcText = this.text.toLowerCase();
+
+    let defaultToYearFirst = extractInput.defaultToYearFirst;
+    let startYear = extractInput.startYear;
+
+    //console.log("extractYearAndRegistrationNumberFromText, lcText is:");
+    //console.log(lcText);
+
+    let regNum = "";
+    let regYear = "";
+
+    function handleTwoExtractedNumbers(num1, num2) {
+      //console.log("num1 is '" + num1 + "'");
+      //console.log("num2 is '" + num2 + "'");
+
+      if (!num1 || !num2 || num1 == lcText || num2 == lcText) {
+        return false;
+      }
+
+      if (num1.length == 4 || num2.length == 4) {
+        let number1 = Number(num1);
+        let number2 = Number(num2);
+        if (defaultToYearFirst) {
+          regYear = num1;
+          regNum = num2;
+        } else {
+          regYear = num2;
+          regNum = num1;
+        }
+        if (!(num2.length == 4 && number2 >= startYear && number2 < 2050)) {
+          if (num1.length == 4 && number1 >= startYear && number1 < 2050) {
+            regYear = num1;
+            regNum = num2;
+          }
+        }
+        return true;
+      }
+      return false;
+    }
+
+    function lookForWellSeparatedNumSlashNum() {
+      //console.log("lcText is '" + lcText + "'");
+
+      const regex = /(?:^.*[^\d\/]|^)(\d+)\s?\/\s?(\d+)(?:[^\d\/].*$|$)/;
+      if (!regex.test(lcText)) {
+        return false;
+      }
+
+      let num1 = lcText.replace(regex, "$1");
+      let num2 = lcText.replace(regex, "$2");
+
+      return handleTwoExtractedNumbers(num1, num2);
+    }
+
+    function lookForNormalNumSlashNum() {
+      let startIndex = lcText.search(/\d+ ?\/ ?\d+/);
+      //console.log("startIndex is: " + startIndex);
+      if (startIndex == -1) {
+        return false;
+      }
+      let refText = lcText.substring(startIndex);
+      let endIndex = refText.search(/[^\d\s\/]/);
+      //console.log("endIndex is: " + endIndex);
+      if (endIndex == -1) {
+        endIndex = refText.length;
+      }
+      refText = refText.substring(0, endIndex).trim();
+
+      //console.log("refText is '" + refText + "'");
+
+      // Note: there could be an extra number on end. e.g.:
+      // victoria (australia) death 1972/29190 79y
+      // refText is '1972/29190 79'
+
+      let num1 = refText.replace(/^(\d+) ?\/ ?\d+\s*\d*$/, "$1");
+      let num2 = refText.replace(/^\d+ ?\/ ?(\d+)\s*\d*$/, "$1");
+
+      return handleTwoExtractedNumbers(num1, num2);
+    }
+
+    function lookForOtherNumSepNum() {
+      // Examples:
+      // Victorian marriage registration 1873,03483,F,Grant,Annie,Box,Alfred John,Edinburgh, 1873,03483,M,Box,Alfred John,Grant,Annie,Gloucestershire,
+      // Victorian death registration 1909,09716,,Box,Annie Cooper,Grant George,Beaton Jane,Melbourne East,60,
+      // Victorian birth registration 1876: 00759,,Smith,Walter,Cornelius,Osullivan Johanna,Br Ea,
+      // Victorian death registration 1876: 00493,,Smith,Walter,Cornelius,Osullivan Johanna,,24D,birth(Gren)
+
+      let startIndex = lcText.search(/registration \d+\s*[:,]\s*\d+/);
+      //console.log("startIndex is: " + startIndex);
+      if (startIndex == -1) {
+        return false;
+      }
+      let refText = lcText.substring(startIndex);
+      let numStartIndex = refText.search(/\d/);
+      if (numStartIndex == -1) {
+        return false;
+      }
+      refText = refText.substring(numStartIndex);
+
+      let endIndex = refText.search(/[^\d\s:,]/);
+      //console.log("endIndex is: " + endIndex);
+      if (endIndex == -1) {
+        endIndex = refText.length;
+      }
+      refText = refText.substring(0, endIndex).trim();
+
+      // refText should now contain the year number, a separator and ref number
+
+      //console.log("refText is '" + refText + "'");
+
+      let num1 = refText.replace(/^[:,\s]*(\d+)\s*[:,]\s*\d+[:,\s]*$/, "$1");
+      let num2 = refText.replace(/^[:,\s]*\d+\s*[:,]\s*(\d+)$/, "$1");
+
+      return handleTwoExtractedNumbers(num1, num2);
+    }
+
+    function lookForSeparateYearAndRefNum() {
+      let regNumIndex = lcText.search(/reg[^\s:.]*[:.\s]*\d+/);
+      if (regNumIndex == -1) {
+        regNumIndex = lcText.search(/reg[^\s:.]*[:.\s]*num[^\s:.]*[:.\s]*\d+/);
+      }
+      if (regNumIndex == -1) {
+        regNumIndex = lcText.search(/ref[^\s:.]*[:.\s]*\d+/);
+      }
+      if (regNumIndex == -1) {
+        regNumIndex = lcText.search(/ref[^\s:.]*[:.\s]*num[^\s:.]*[:.\s]*\d+/);
+      }
+      if (regNumIndex == -1) {
+        return false;
+      }
+      let regNumPart = lcText.substring(regNumIndex);
+      let regNumStartIndex = regNumPart.search(/\d/);
+      if (regNumStartIndex == -1) {
+        return false;
+      }
+      regNumPart = regNumPart.substring(regNumStartIndex);
+      let regNumEndIndex = regNumPart.search(/[^\d]+/);
+      if (regNumEndIndex == -1) {
+        regNumEndIndex = regNumPart.length;
+      }
+      regNum = regNumPart.substring(0, regNumEndIndex);
+
+      // now try to find year
+      let regYearIndex = lcText.search(/reg[^\s:.]*[:.\s]*date[^\s:.]*[:.\s]*\d\d\d\d/);
+      if (regYearIndex == -1) {
+        regYearIndex = lcText.search(/reg[^\s:.]*[:.\s]*year[^\s:.]*[:.\s]*\d\d\d\d/);
+      }
+      if (regYearIndex == -1) {
+        regYearIndex = lcText.search(/year[:.\s]*[\sa-z]*\d\d\d\d/);
+      }
+
+      // try to find year in a narrative type string or data string
+      if (regYearIndex == -1) {
+        regYearIndex = lcText.search(/born[:.\s]*[\sa-z]*\d\d\d\d/);
+      }
+      if (regYearIndex == -1) {
+        regYearIndex = lcText.search(/birth[:.\s]*[\sa-z]*\d\d\d\d/);
+      }
+      if (regYearIndex == -1) {
+        regYearIndex = lcText.search(/died[:.\s]*[\sa-z]*\d\d\d\d/);
+      }
+      if (regYearIndex == -1) {
+        regYearIndex = lcText.search(/death[:.\s]*[\sa-z]*\d\d\d\d/);
+      }
+      if (regYearIndex == -1) {
+        regYearIndex = lcText.search(/married[:.\s]*[\sa-z]*\d\d\d\d/);
+      }
+      if (regYearIndex == -1) {
+        regYearIndex = lcText.search(/marriage[:.\s]*[\sa-z]*\d\d\d\d/);
+      }
+
+      if (regYearIndex == -1) {
+        return false;
+      }
+      let regYearPart = lcText.substring(regYearIndex);
+      let regYearStartIndex = regYearPart.search(/\d/);
+      if (regYearStartIndex == -1) {
+        return false;
+      }
+      regYearPart = regYearPart.substring(regYearStartIndex);
+      let regYearEndIndex = regYearPart.search(/[^\d]+/);
+      if (regYearEndIndex == -1) {
+        regYearEndIndex = regYearPart.length;
+      }
+      regYear = regYearPart.substring(0, regYearEndIndex);
+      return true;
+    }
+
+    let foundYearAndNum = lookForWellSeparatedNumSlashNum();
+
+    if (!foundYearAndNum) {
+      foundYearAndNum = lookForNormalNumSlashNum();
+    }
+
+    if (!foundYearAndNum) {
+      foundYearAndNum = lookForOtherNumSepNum();
+    }
+
+    if (!foundYearAndNum) {
+      foundYearAndNum = lookForSeparateYearAndRefNum();
+    }
+
+    //console.log("regNum is '" + regNum + "'");
+    //console.log("regYear is '" + regYear + "'");
+
+    if (!foundYearAndNum) {
+      return undefined;
+    }
+
+    return { regYear: regYear, regNum: regNum };
+  }
+}
+
+export { CitationParser };
---
+++ extension/site/ancestry/core/ancestry_context.mjs
@@ -0,0 +1,255 @@
+/*
+MIT License
+
+Copyright (c) 2020 Robert M Pavey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+function transformLink(linkText, phase, options) {
+  // linkUrl: "https://ancestry.prf.hn/click/camref:1011l4xx5/type:cpc/destination:https://search.ancestry.com/cgi-bin/sse.dll?indiv=1&db=2352&h=1903048"
+
+  // In preview mode:
+  // linkUrl: "https://search.ancestry.co.uk/cgi-bin/sse.dll?indiv=1&db=7619&h=4576311"
+  // when saved:
+  // linkUrl: "https://ancestry.prf.hn/click/camref:1011l4JYM/type:cpc/destination:https://search.ancestry.co.uk/cgi-bin/sse.dll?indiv=1&db=7619&h=4576311"
+  // In preview mode:
+  // linkUrl: "https://search.ancestry.com/cgi-bin/sse.dll?indiv=1&db=7619&h=4576311"
+  // When saved:
+  // linkUrl: "https://ancestry.prf.hn/click/camref:1011l4xx5/type:cpc/destination:https://search.ancestry.com/cgi-bin/sse.dll?indiv=1&db=7619&h=4576311"
+
+  let link = linkText;
+
+  if (phase != 1) {
+    return "";
+  }
+
+  if (!/ancestry/i.test(linkText)) {
+    return "";
+  }
+
+  // do not redirect sharing links when using library edition since that does not work
+  if (link.includes("/sharing/") || link.includes("%2Fsharing%2F")) {
+    let desiredDomain = options.search_ancestry_domain;
+    const tabOption = options.context_general_newTabPos;
+    if (desiredDomain.includes("library")) {
+      return link;
+    }
+  }
+
+  //console.log("openAncestryLink, link is: " + link);
+  if (link.startsWith("https://click.linksynergy")) {
+    // NOTE: Ancestry switched from Partnerize to Rakuten on 18 Apr 2023. Partnerize used the
+    // "prf.hn" format, Rakuten uses this new click.linksynergy format.
+    // And example link in this format:
+    // https://click.linksynergy.com/deeplink?id=Xib7NfnK11s&amp;mid=50138&amp;murl=https%3A%2F%2Fsearch.ancestry.com%2Fcgi-bin%2Fsse.dll%3Findiv%3D1%26db%3D61596%26h%3D8900762
+    let linkStartText = "https%3A%2F%2Fsearch.ancestry";
+    let realLinkIndex = link.indexOf(linkStartText);
+    if (realLinkIndex == -1) {
+      linkStartText = "https%3A%2F%2Fwww.ancestry";
+      realLinkIndex = link.indexOf(linkStartText);
+    }
+    //console.log("openAncestryLink, realLinkIndex is: " + realLinkIndex);
+    if (realLinkIndex != -1) {
+      let domainEndIndex = link.indexOf("%2F", realLinkIndex + linkStartText.length);
+      if (domainEndIndex != -1) {
+        let linkStart = link.substring(realLinkIndex, domainEndIndex);
+        // linkStart = https%3A%2F%2Fsearch.ancestry.com for example
+        let ancestryStartIndex = linkStart.indexOf("ancestry");
+        if (ancestryStartIndex != -1) {
+          let domain = linkStart.substring(ancestryStartIndex);
+          //console.log("openAncestryLink, domain is: " + domain);
+
+          let desiredDomain = options.search_ancestry_domain;
+          //console.log("openAncestryLink, desiredDomain is: " + desiredDomain);
+
+          if (desiredDomain != "none" && desiredDomain != domain) {
+            // we want to change the link, first decide if we are changing to a domain supported by
+            // the Rakuten links
+            const rukutenSupportedDomains = [
+              { domain: "ancestry.com", mid: "50138" },
+              { domain: "ancestry.co.uk", mid: "50140" },
+              { domain: "ancestry.ca", mid: "50139" },
+              { domain: "ancestry.com.au", mid: "50142" },
+            ];
+            let mid = "";
+            for (let entry of rukutenSupportedDomains) {
+              if (entry.domain == desiredDomain) {
+                mid = entry.mid;
+                break;
+              }
+            }
+            if (mid) {
+              // it is a supported domain to modify the mid and domain in link
+              let newLink = link.replace(domain, desiredDomain);
+              if (newLink && newLink != link) {
+                link = newLink;
+                //console.log("openAncestryLink, new link is: " + newLink);
+              }
+              newLink = link.replace(/([&;])mid=\d+/, "$1mid=" + mid);
+              if (newLink && newLink != link) {
+                link = newLink;
+                //console.log("openAncestryLink, new link is: " + newLink);
+              }
+            } else {
+              // not one of the supported domains - change to a non-referral link
+              let encodedPlainLink = link.substring(realLinkIndex);
+              //console.log("openAncestryLink, encodedPlainLink is: " + encodedPlainLink);
+              let plainLink = decodeURIComponent(encodedPlainLink);
+              //console.log("openAncestryLink, plainLink is: " + plainLink);
+              let newLink = plainLink.replace(domain, desiredDomain);
+              if (newLink && newLink != plainLink) {
+                link = newLink;
+                //console.log("openAncestryLink, new link is: " + newLink);
+              }
+            }
+          }
+        }
+      }
+    }
+  } else if (link.startsWith("https://ancestry.prf.hn/")) {
+    // NOTE: Ancestry switched from Partnerize to Rakuten on 18 Apr 2023. Partnerize used the
+    // "prf.hn" format. So this is no longer used.
+    let linkStartText = "https://search.ancestry";
+    let realLinkIndex = link.indexOf(linkStartText);
+    if (realLinkIndex == -1) {
+      linkStartText = "https://www.ancestry";
+      realLinkIndex = link.indexOf(linkStartText);
+    }
+    //console.log("openAncestryLink, realLinkIndex is: " + realLinkIndex);
+    if (realLinkIndex != -1) {
+      let domainEndIndex = link.indexOf("/", realLinkIndex + linkStartText.length);
+      if (domainEndIndex != -1) {
+        let linkStart = link.substring(realLinkIndex, domainEndIndex);
+        // linkStart = https://search.ancestry.com for example
+        let ancestryStartIndex = linkStart.indexOf("ancestry");
+        if (ancestryStartIndex != -1) {
+          let domain = linkStart.substring(ancestryStartIndex);
+          //console.log("openAncestryLink, domain is: " + domain);
+
+          let desiredDomain = options.search_ancestry_domain;
+          //console.log("openAncestryLink, desiredDomain is: " + desiredDomain);
+
+          if (desiredDomain != "none" && desiredDomain != domain) {
+            let newLink = link.replace(domain, desiredDomain);
+            if (newLink && newLink != link) {
+              link = newLink;
+              //console.log("openAncestryLink, new link is: " + newLink);
+            }
+          }
+        }
+      }
+    }
+  } else {
+    // could of form:
+    // https://www.ancestry.co.uk/discoveryui-content/view/19270862:1623?tid=&pid=&queryId=126e8ebdb5f54d54f60534d57389787f&_phsrc=Vww4&_phstart=successSource
+    // or:
+    // https://search.ancestry.co.uk/cgi-bin/sse.dll?db=uki1861&indiv=try&h=19053031
+
+    let domain = link.replace(/^https?\:\/\/[^\.]+\.(ancestry[^\/]+)\/.*/, "$1");
+    //console.log("openAncestryLink, domain is: " + domain);
+    if (domain && domain != link) {
+      let desiredDomain = options.search_ancestry_domain;
+      //console.log("openAncestryLink, desiredDomain is: " + desiredDomain);
+
+      if (desiredDomain != "none" && desiredDomain != domain) {
+        let newLink = link.replace(domain, desiredDomain);
+        if (newLink && newLink != link) {
+          link = newLink;
+          //console.log("openAncestryLink, new link is: " + newLink);
+        }
+      }
+    }
+  }
+
+  return link;
+}
+
+function transformTemplateToLink(text, options) {
+  const templateRegex = /ancestry/i;
+  if (!templateRegex.test(text)) {
+    return "";
+  }
+
+  let desiredDomain = options.search_ancestry_domain;
+  if (!desiredDomain || desiredDomain == "none") {
+    desiredDomain = "ancestry.com";
+  }
+
+  let link = "";
+
+  if (text.includes("Ancestry Record")) {
+    // {{Ancestry Record|1989|219120}}
+    let dbId = text.replace(/\{\{Ancestry Record\|([^|]+)\|[^}]+[^|}]*\}\}/, "$1");
+    let recordId = text.replace(/\{\{Ancestry Record\|[^|]+\|([^|}]+)[^}]*\}\}/, "$1");
+    if (dbId && dbId != text && recordId && recordId != text) {
+      if (/^\d+$/.test(dbId)) {
+        // This only works when the dbId is a number
+        // https://www.ancestry.com/discoveryui-content/view/219120:1989
+        link = "https://www." + desiredDomain + "/discoveryui-content/view/" + recordId + ":" + dbId;
+      } else {
+        // https://search.ancestry.com/cgi-bin/sse.dll?indiv=1&db=uki1851&h=9872118
+        link = "https://search." + desiredDomain + "/cgi-bin/sse.dll?indiv=1&db=" + dbId + "&h=" + recordId;
+      }
+    }
+  } else if (text.includes("Ancestry Image")) {
+    // {{Ancestry Image|1234|5678}}
+    let num1 = text.replace(/\{\{Ancestry Image\|([^|]+)\|[^|}]+[^}]*\}\}/, "$1");
+    let num2 = text.replace(/\{\{Ancestry Image\|[^|]+\|([^|}]+)[^}]*\}\}/, "$1");
+    if (num1 && num1 != text && num2 && num2 != text) {
+      // https://www.ancestry.com/interactive/1234/5678
+      link = "https://www." + desiredDomain + "/interactive/" + num1 + "/" + num2;
+    }
+  } else if (text.includes("Ancestry Sharing")) {
+    // {{Ancestry Sharing|26032935|f25069}}
+    // or
+    // {{Ancestry Sharing|360708|45105e24a959143273cc624d7eeb616ab4e5002d2c58dca23a9a27d6088b2a7b}}
+    let num1 = text.replace(/\{\{Ancestry Sharing\|([^|]+)\|[^}|]+[^}]*\}\}/, "$1");
+    let num2 = text.replace(/\{\{Ancestry Sharing\|[^|]+\|([^}|]+)[^}]*\}\}/, "$1");
+    if (num1 && num1 != text && num2 && num2 != text) {
+      if (num2.length > 10) {
+        // https://www.ancestry.com/sharing/360708?token=45105e24a959143273cc624d7eeb616ab4e5002d2c58dca23a9a27d6088b2a7b
+        link = "https://www." + desiredDomain + "/sharing/" + num1 + "?token=" + num2;
+      } else {
+        // https://www.ancestry.com/sharing/26032935?h=f25069
+        link = "https://www." + desiredDomain + "/sharing/" + num1 + "?h=" + num2;
+      }
+    }
+  } else if (text.includes("Ancestry Tree Media")) {
+    // {{Ancestry Tree Media|86808578|d69a7d6a-c773-48b1-ab09-19100cd55c14}}
+    let num1 = text.replace(/\{\{Ancestry Tree Media\|([^|]+)\|[^}|]+[^}]*\}\}/, "$1");
+    let num2 = text.replace(/\{\{Ancestry Tree Media\|[^|]+\|([^}|]+)[^}]*\}\}/, "$1");
+    if (num1 && num1 != text && num2 && num2 != text) {
+      // https://www.ancestry.com/family-tree/tree/86808578/media/d69a7d6a-c773-48b1-ab09-19100cd55c14
+      link = "https://www." + desiredDomain + "/family-tree/tree/" + num1 + "/media/" + num2;
+    }
+  } else if (text.includes("Ancestry Tree")) {
+    // {{Ancestry Tree|1234|5678}}
+    let num1 = text.replace(/\{\{Ancestry Tree\|([^|]+)\|[^}|]+[^}]*\}\}/, "$1");
+    let num2 = text.replace(/\{\{Ancestry Tree\|[^|]+\|([^}|]+)[^}]*\}\}/, "$1");
+    if (num1 && num1 != text && num2 && num2 != text) {
+      // https://www.ancestry.com/family-tree/person/tree/1234/person/19531917216/facts
+      link = "https://www." + desiredDomain + "/family-tree/person/tree/" + num1 + "/person/" + num2 + "/facts";
+    }
+  }
+
+  return link;
+}
+
+export { transformLink, transformTemplateToLink };
