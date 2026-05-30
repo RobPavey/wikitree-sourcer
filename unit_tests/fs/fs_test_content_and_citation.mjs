05ea88f2e9561cc9b69253827fda05d01b53af07
Rob Pavey <robpavey@gmail.com>
2026-05-29 17:15:30 -0700

    fs: fix error I introduce in source test cases

--- unit_tests/fs/fs_test_content_and_citation.mjs
+++ unit_tests/fs/fs_test_content_and_citation.mjs
@@ -822,13 +822,17 @@ const optionVariants = [
 function cleanStaleOutputFiles(testManager) {
   //
   let logger = new LocalErrorLogger(testManager.results, "fs_clean");
-  let testCaseSets = [regressionData, imageRegressionData, personRegressionData, bookRegressionData];
+  let testCaseSets = [
+    regressionData,
+    imageRegressionData,
+    personRegressionData,
+    bookRegressionData,
+    createSourceRegressionData,
+  ];
   removeStaleOutputFiles("fs", "extracted_data", testCaseSets, logger);
   removeStaleOutputFiles("fs", "generalized_data", testCaseSets, logger);
   removeStaleOutputFiles("fs", "citations", testCaseSets, logger);
   removeStaleOutputFiles("fs", "household_tables", testCaseSets, logger);
-
-  removeStaleOutputFiles("fs", "extracted_data", createSourceRegressionData, logger);
 }
 
 async function runTests(testManager) {
